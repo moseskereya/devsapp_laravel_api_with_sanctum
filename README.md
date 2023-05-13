3ab93121a1d72294f1ac64ace2ac077587e86519
Moses <lazaromoses77@gmail.com>
2023-05-13 14:50:56 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+Hello frinds, in today we are going to create and secure a restful api using sactum and laravel.
+Please feel free to use some of these resources whenever you need em!
---
+++ routes/api.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\ProductController;
+use App\Models\Product;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+//public routes
+ Route::post('/register', [AuthController::class, 'register']);
+ Route::post('/login', [AuthController::class, 'login']);
+ Route::get('/products', [ProductController::class, 'index']);
+ Route::get('/products/{id}', [ProductController::class, 'show']);
+ Route::get('/products/search/{name}', [ProductController::class, 'search']);
+
+Route::group(['middleware' => ['auth:sanctum']],  function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::post('/products', [ProductController::class, 'store']);
+    Route::put('/products/{id}', [ProductController::class, 'update']);
+    Route::delete('/products/{id}', [ProductController::class, 'destroy']);
+});
+
+
+
